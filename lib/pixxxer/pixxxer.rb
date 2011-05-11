85020a8030a94c7eff3831428e0b7cb674fb5c90
Guy Royse <guy@guyroyse.com>
2011-05-11 17:25:31 -0400

    moved builder code to seperate class

--- lib/pixxxer/pixxxer.rb
+++ lib/pixxxer/pixxxer.rb
@@ -1,11 +1,54 @@
 $pixxxer_templates = {}
 
+class PixxxerFieldPixxxitter
+	def initialize(field)
+		@field = field
+	end
+	def pixxxit(hash, record)
+		field = fetch_field hash
+		add_to_record record, field
+	end
+	def add_to_record(record, field)
+		record = widen_record record
+		inject_field record, field
+	end
+	def widen_record(record)
+		record.ljust @field.position
+	end
+	def inject_field(record, field)
+		record[@field.position, field.length] = field
+		record
+	end
+	def fetch_field(hash)
+		pad_string shorten_string(hash[@field.name].to_s)
+	end
+	def shorten_string(string)
+		if @field.type == Integer
+			return string[string.length - @field.width, @field.width] unless @field.width.nil?
+		else
+			return string[0, @field.width] unless @field.width.nil?
+		end
+		string
+	end
+	def pad_string(string)
+		if @field.type == Integer
+			return string.rjust(@field.width, '0') unless @field.width.nil?
+		else
+			return string.ljust(@field.width, ' ') unless @field.width.nil?
+		end
+		string
+	end
+
+end
+
 class PixxxerField
+	attr_reader :width, :name, :position, :type, :precision
 	def initialize(field_name, template)
 		@template = template
 		@name = field_name
 		@position = 0
 		@precision = 0
+		@pixxxitter = PixxxerFieldPixxxitter.new self
 	end
 	def with_width(width)
 		@width = width
@@ -33,43 +76,12 @@ class PixxxerField
 	def and
 		@template
 	end
+	def pixxxit(hash, record)
+		@pixxxitter.pixxxit hash, record
+	end
 	def depixxxit(string)
 		field = extract_field string
 		coerce_field field
-	end
-	def pixxxit(hash, record)
-		field = fetch_field hash
-		add_to_record record, field
-	end
-	def add_to_record(record, field)
-		record = widen_record record
-		inject_field record, field
-	end
-	def widen_record(record)
-		record.ljust @position
-	end
-	def inject_field(record, field)
-		record[@position, field.length] = field
-		record
-	end
-	def fetch_field(hash)
-		pad_string shorten_string(hash[@name].to_s)
-	end
-	def shorten_string(string)
-		if @type == Integer
-			return string[string.length - @width, @width] unless @width.nil?
-		else
-			return string[0, @width] unless @width.nil?
-		end
-		string
-	end
-	def pad_string(string)
-		if @type == Integer
-			return string.rjust(@width, '0') unless @width.nil?
-		else
-			return string.ljust(@width, ' ') unless @width.nil?
-		end
-		string
 	end
 	def extract_field(string)
 		return string[@position...string.length] if @width.nil?
